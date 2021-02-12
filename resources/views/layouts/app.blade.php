d2091db70d055bb73677ac1ecf6e8ba5dfa3698b
Nick Jay <b.nickjay05@gmail.com>
2021-02-12 16:36:52 +0800

    embed js script for provely

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -135,7 +135,7 @@
 
     </script>
     <script>(function(w, d) { w.CollectId = "6020ba3c8255d2479d733937"; var h = d.head || d.getElementsByTagName("head")[0]; var s = d.createElement("script"); s.setAttribute("type", "text/javascript"); s.async=true; s.setAttribute("src", "https://collectcdn.com/launcher.js"); h.appendChild(s); })(window, document);</script>
-    <!-- Provely Conversions App Data Code -->
+    <!-- Provely Conversions App Display Code -->
 <script>(function(w,n) {
     if (typeof(w[n]) == 'undefined'){ob=n+'Obj';w[ob]=[];w[n]=function(){w[ob].push(arguments);};
     d=document.createElement('script');d.type = 'text/javascript';d.async=1;
@@ -144,9 +144,9 @@
     provelys('config', 'baseUrl', 'app.provely.io');
     provelys('config', 'https', 1);
     provelys('data', 'campaignId', '26321');
-    provelys('config', 'track', 1);
+    provelys('config', 'widget', 1);
     </script>
-    <!-- End Provely Conversions App Data Code -->
+    <!-- End Provely Conversions App Display Code -->
     @yield('js')
 </body>
 
